dc77a303684e5c695b20e171c773da237897ed7e
SPlay <SPlay@DESKTOP-U92BQFG>
2018-12-17 18:28:10 -0200

    Add project files.

---
+++ GetValuesFromDictionaryInCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GetValuesFromDictionaryInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var namesAndAges = new Dictionary<string, int>();
+            namesAndAges["Roberto"] = 17;
+            namesAndAges["Helena"] = 19;
+            //
+            foreach (var item in namesAndAges)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+        }
+    }
+}
